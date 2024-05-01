4e7c10a197190ffbf35a5ec9d2ece514df612e4c
Mengxin1437 <3386716762@qq.com>
2024-05-01 18:35:21 +0800

    '2024.05.01/18.35'

--- 保存脚本.bat
+++ 保存脚本.bat
@@ -3,7 +3,7 @@ git status
 git add .
 git status
 ::获取当前系统时间并指定格式（提取信息）
-set name=%date:~0,4%.%date:~5,2%.%date:~8,2%/%time:~0,2%.%time:~3,2%shao
+set name=%date:~0,4%.%date:~5,2%.%date:~8,2%/%time:~0,2%.%time:~3,2%
 ::去掉空格
 set "name=%name: =%"
 git commit -m '%name%'
