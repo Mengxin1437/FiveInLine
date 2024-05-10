b52220d69172d2010a53214b34fbabd33731d1fd
Mengxin1437 <3386716762@qq.com>
2024-05-10 17:27:10 +0800

    '2024.05.10/17.27'

--- src/graphic/awt/MyMouseEvent.java
+++ src/graphic/awt/MyMouseEvent.java
@@ -5,15 +5,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MyMouseEvent extends MouseAdapter {
-    private final Frame frame;
-    public MyMouseEvent(Frame frame){
-        this.frame = frame;
-    }
-
     @Override
     public void mouseReleased(MouseEvent e) {
         super.mouseReleased(e);
         //使用e.getX();e.getY()获取横纵坐标并转化到棋盘的坐标
-
+        System.out.println(e.getX()+" "+e.getY());
     }
 }
