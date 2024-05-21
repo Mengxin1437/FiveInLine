315c006999ca126d1a4de107fcbc90e5244924b6
Mengxin1437 <3386716762@qq.com>
2024-05-21 23:48:45 +0800

    '2024.05.10/17.29'

--- src/graphic/swing/Login.java
+++ src/graphic/swing/Login.java
@@ -29,6 +29,19 @@ public class Login extends JFrame {
         this.setExtendedState(JFrame.ICONIFIED);
     }
 
+    private void btnLogin(ActionEvent e) {
+        Container container = this.getContentPane();
+        CardLayout cardLayout = (CardLayout)container.getLayout();
+        cardLayout.next(container);
+    }
+
+    private void btnBack(ActionEvent e) {
+        Container container = this.getContentPane();
+        CardLayout cardLayout = (CardLayout)container.getLayout();
+        cardLayout.previous(container);
+    }
+
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         panel1 = new JPanel();
@@ -36,11 +49,20 @@ public class Login extends JFrame {
         btnMinimize = new JButton();
         tfUsername = new RoundTextField("用户名", 25);
         tfPassword = new RoundTextField("密码", 25);
+        lbHead = new Headshot(this);
         checkBox1 = new JCheckBox();
         checkBox2 = new JCheckBox();
         btnLogin = new JButton();
-        lbHead = new Headshot(this);
         background = new JLabel();
+        panel2 = new JPanel();
+        btnMinimize2 = new JButton();
+        btnClose2 = new JButton();
+        button1 = new JButton();
+        button2 = new JButton();
+        radioButton1 = new JRadioButton();
+        radioButton2 = new JRadioButton();
+        btnBack = new JButton();
+        background2 = new JLabel();
 
         //======== this ========
         setResizable(false);
@@ -91,6 +113,11 @@ public class Login extends JFrame {
             panel1.add(tfPassword);
             tfPassword.setBounds(new Rectangle(new Point(50, 240), tfPassword.getPreferredSize()));
 
+            //---- lbHead ----
+            lbHead.setPreferredSize(new Dimension(60, 60));
+            panel1.add(lbHead);
+            lbHead.setBounds(new Rectangle(new Point(120, 120), lbHead.getPreferredSize()));
+
             //---- checkBox1 ----
             checkBox1.setText("\u8bb0\u4f4f\u5bc6\u7801");
             checkBox1.setBackground(new Color(0x002b2d30, true));
@@ -114,13 +141,9 @@ public class Login extends JFrame {
             btnLogin.setPreferredSize(new Dimension(90, 30));
             btnLogin.setBorder(null);
             btnLogin.setFocusPainted(false);
+            btnLogin.addActionListener(e -> btnLogin(e));
             panel1.add(btnLogin);
             btnLogin.setBounds(new Rectangle(new Point(105, 325), btnLogin.getPreferredSize()));
-
-            //---- lbHead ----
-            lbHead.setPreferredSize(new Dimension(60, 60));
-            panel1.add(lbHead);
-            lbHead.setBounds(new Rectangle(new Point(120, 120), lbHead.getPreferredSize()));
 
             //---- background ----
             background.setPreferredSize(new Dimension(300, 420));
@@ -144,8 +167,108 @@ public class Login extends JFrame {
             }
         }
         contentPane.add(panel1, "card1");
+
+        //======== panel2 ========
+        {
+            panel2.setPreferredSize(new Dimension(300, 420));
+            panel2.setLayout(null);
+
+            //---- btnMinimize2 ----
+            btnMinimize2.setText("-");
+            btnMinimize2.setFont(new Font("Inter", Font.PLAIN, 20));
+            btnMinimize2.setPreferredSize(new Dimension(40, 25));
+            btnMinimize2.setBackground(new Color(0x002b2d30, true));
+            btnMinimize2.setForeground(Color.black);
+            btnMinimize2.setBorder(null);
+            btnMinimize2.setFocusPainted(false);
+            btnMinimize2.addActionListener(e -> btnMinimize(e));
+            panel2.add(btnMinimize2);
+            btnMinimize2.setBounds(220, 0, 40, 25);
+
+            //---- btnClose2 ----
+            btnClose2.setText("\u00d7");
+            btnClose2.setBackground(new Color(0x002b2d30, true));
+            btnClose2.setForeground(Color.black);
+            btnClose2.setBorder(null);
+            btnClose2.setFocusPainted(false);
+            btnClose2.setFont(new Font("Inter", Font.PLAIN, 20));
+            btnClose2.setPreferredSize(new Dimension(40, 25));
+            btnClose2.addActionListener(e -> btnClose(e));
+            panel2.add(btnClose2);
+            btnClose2.setBounds(260, 0, 40, 25);
+
+            //---- button1 ----
+            button1.setText("\u8054\u673a\u5bf9\u6218");
+            button1.setPreferredSize(new Dimension(100, 30));
+            button1.setBorder(null);
+            button1.setBorderPainted(false);
+            panel2.add(button1);
+            button1.setBounds(105, 210, 100, 30);
+
+            //---- button2 ----
+            button2.setText("\u5c40\u57df\u7f51\u5bf9\u6218");
+            button2.setPreferredSize(new Dimension(100, 30));
+            button2.setBorder(null);
+            button2.setBorderPainted(false);
+            panel2.add(button2);
+            button2.setBounds(105, 260, 100, 30);
+
+            //---- radioButton1 ----
+            radioButton1.setText("\u4e94\u5b50\u68cb");
+            radioButton1.setForeground(Color.black);
+            radioButton1.setBackground(new Color(0x000d4b7c, true));
+            radioButton1.setFocusPainted(false);
+            radioButton1.setSelected(true);
+            panel2.add(radioButton1);
+            radioButton1.setBounds(75, 140, 61, 21);
+
+            //---- radioButton2 ----
+            radioButton2.setText("\u9ed1\u767d\u68cb");
+            radioButton2.setForeground(Color.black);
+            radioButton2.setBackground(new Color(0x000d4b7c, true));
+            radioButton2.setFocusPainted(false);
+            panel2.add(radioButton2);
+            radioButton2.setBounds(180, 140, 61, 21);
+
+            //---- btnBack ----
+            btnBack.setText("\u2190\u8fd4\u56de");
+            btnBack.setOpaque(false);
+            btnBack.setBackground(new Color(0x002b2d30, true));
+            btnBack.setFocusPainted(false);
+            btnBack.setBorderPainted(false);
+            btnBack.addActionListener(e -> btnBack(e));
+            panel2.add(btnBack);
+            btnBack.setBounds(new Rectangle(new Point(0, 390), btnBack.getPreferredSize()));
+
+            //---- background2 ----
+            background2.setPreferredSize(new Dimension(300, 420));
+            background2.setIcon(new ImageIcon(getClass().getResource("/R.gif")));
+            panel2.add(background2);
+            background2.setBounds(0, 0, 300, 420);
+
+            {
+                // compute preferred size
+                Dimension preferredSize = new Dimension();
+                for(int i = 0; i < panel2.getComponentCount(); i++) {
+                    Rectangle bounds = panel2.getComponent(i).getBounds();
+                    preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                    preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                }
+                Insets insets = panel2.getInsets();
+                preferredSize.width += insets.right;
+                preferredSize.height += insets.bottom;
+                panel2.setMinimumSize(preferredSize);
+                panel2.setPreferredSize(preferredSize);
+            }
+        }
+        contentPane.add(panel2, "card2");
         pack();
         setLocationRelativeTo(getOwner());
+
+        //---- buttonGroup1 ----
+        var buttonGroup1 = new ButtonGroup();
+        buttonGroup1.add(radioButton1);
+        buttonGroup1.add(radioButton2);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
@@ -155,10 +278,19 @@ public class Login extends JFrame {
     private JButton btnMinimize;
     private JTextField tfUsername;
     private JTextField tfPassword;
+    private JLabel lbHead;
     private JCheckBox checkBox1;
     private JCheckBox checkBox2;
     private JButton btnLogin;
-    private JLabel lbHead;
     private JLabel background;
+    private JPanel panel2;
+    private JButton btnMinimize2;
+    private JButton btnClose2;
+    private JButton button1;
+    private JButton button2;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JButton btnBack;
+    private JLabel background2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
