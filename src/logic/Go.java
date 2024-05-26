af7f9e8b5486dd152dbddf8c4e5ab08ad406455c
Mengxin1437 <3386716762@qq.com>
2024-05-26 18:54:09 +0800

    添加了围棋的能否落子的判断

--- src/logic/Go.java
+++ src/logic/Go.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Go extends Chess{
+    private Point banPos; //禁着点
+
+    @Override
+    protected void initBoard(int row, int column) {
+        super.initBoard(row, column);
+        banPos = null;
+    }
+
     @Override
     public Boolean isWin(int x, int y) {
         return null;
@@ -12,13 +20,76 @@ public class Go extends Chess{
 
     @Override
     public boolean isMovePositionOk(int x, int y) {
-        return super.isMovePositionOk(x, y);
+        if(!super.isMovePositionOk(x, y)) return false;
+        boolean result = true;
+        //合法性判断
+
+        //1.不能下在让自己方棋子没气的位置
+        boolean res1, res2=false, res3, res4=false;
+        /*试验性落子*/
+        board[x][y] = turn;
+        HashSet<Point> tribeSelf = getTribe(x, y); //获取落子后所处的部落
+        HashSet<Point> gasSelf = getGas(tribeSelf);
+        if(gasSelf.isEmpty()){
+            res1 = false; //下了之后自己没气了
+        }else{
+            res1 = true;
+        }
+
+        //2.当下的位置可以让对方棋子被提子时规则1不成立
+        ArrayList<Point> around = getAllAround(x, y);
+        for(Point p: around){
+            if(board[p.x][p.y] != null && board[p.x][p.y] != board[x][y]){
+                HashSet<Point> enemyTribe = getTribe(p.x, p.y);
+                HashSet<Point> enemyGas = getGas(enemyTribe);
+                if(enemyGas.isEmpty()){
+                    res2 = true; //下了之后可以提掉对方的棋子
+                    break;
+                }
+            }
+        }
+
+        //3.当下的位置是禁着点时2不成立
+        if(banPos==null){
+            res3 = false; //不是禁着点
+        }else{
+            res3 = banPos.equals(new Point(x, y));
+        }
+
+        //4.当下的位置可以让对方提多个棋子时禁着点不成立
+        for(Point p: around){
+            if(board[p.x][p.y] != null && board[p.x][p.y] != board[x][y]){
+                HashSet<Point> enemyTribe = getTribe(p.x, p.y);
+                HashSet<Point> enemyGas = getGas(enemyTribe);
+                if(enemyGas.isEmpty() && enemyTribe.size()>1){
+                    res4 = true; //可以提掉对方的多个棋子
+                    break;
+                }
+            }
+        }
+
+        /*还原*/
+        board[x][y] = null;
+
+//        if(res4){
+//            return true;
+//        }else{
+//            if(res3){
+//                return false;
+//            }else{
+//                if(res2) return true;
+//                else return res1;
+//            }
+//        }
+        //简化后的
+        return res4 || (!res3 && (res2 || res1));
     }
 
     @Override
     public void moveDown(int x, int y) {
         super.moveDown(x, y); //会直接改变board并交换turn
 
+        banPos = null; //落子后本轮的禁着点就无效了
         ArrayList<Point> around = getAllAround(x, y);
         for(Point p: around){
             if(board[p.x][p.y] != null && board[p.x][p.y] != board[x][y]){//敌方棋子
@@ -28,6 +99,10 @@ public class Go extends Chess{
                     //提子
                     for(Point pt: tribe){
                         board[pt.x][pt.y] = null;
+                    }
+                    // 被提的棋子只有一个时可能出现禁着点
+                    if(tribe.size() == 1){
+                        banPos = tribe.iterator().next();
                     }
                 }
             }
